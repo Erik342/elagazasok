77991ca01e8299182bf6dc4dc9761eb360a4ceb3
Erik342 <zirklerik@gmail.com>
2025-02-17 13:50:42 +0100

    Egybeágyazott elágazások

--- src/elagazasok/Elagazasok.java
+++ src/elagazasok/Elagazasok.java
@@ -18,35 +18,100 @@ public class Elagazasok {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int szam = 5;
-        if (szam >=0)
-            System.out.println("A szam pozitiv!");
+        int gergo = 23;
+
+      // Elágazások
+      if (gergo >= 0){
+          System.out.println("A szam pozitiv");
+      }
+      else{
+          System.out.println("A szam negativ");
+      }
+
+      // Különben
+      boolean feltetel = (gergo >= 0);
+      // Ha igaz
+      // feltetel = true
+      // feltetel = false
+      if (feltetel){
+          System.out.println("A szam pozitiv");
+      }
+      // Különben
+      else{
+          System.out.println("A szam negativ");
+      }
+
+      // Összehasonlító műveletek
+      /*
+      Egyenlőség: ==
+      Tagadás: !
+      Nem egyenlő: !=
+      Kisebb: a < b
+      Kisebb vagy egyenlő: a <= b
+      Nagyobb: a > b
+      Nagyobb vagy egyenlő: a >= b */
+
+      // Kérjünk be két számot, írjuk ki a nagyobbat
+      System.out.println("Kerek egy szamot: ");
+      Scanner be = new Scanner(System.in); // Adatbekéréshez
+      int a = be.nextInt(); // be.nextInt() számot vár a felhasználótól
+      System.out.println("Kerek egy masik szamot: ");
+      int b = be.nextInt();
+      // Döntsük el, hogy melyik nagyobb
+      if(a > b){
+          System.out.printf("Az %d nagyobb, mint a %d", a, b);
+      }
+      else{
+          System.out.printf("Az %d nagyobb, mint a %d", b, a);
+      }
+
+      // Logikai műveletek
+      /*!: tagadás, nem&&: logikai ÉS: logikai VAGY
+
+      */
+
+      // (2 > 3) && (3 > 2) => Hamis
+      // (3 > 2)  (2 > 3) => Igaz
+      int egesz = 30;
+      if(egesz % 10 == 0 && egesz % 3 == 0){
+          System.out.println("A 30 oszthato 10-zel es 3-mal maradék nelkul.");}
+      else{
+          System.out.println("A 30 nem oszthato maradek nelkul 10-zel és 3-mal");}
+      egesz = 27;
+      if (egesz % 10 == 0 || egesz % 3 == 0){
+          System.out.println("A 27 oszthato 10-zel vagy 3-mal maradeknelkul");}
+      else{
+          System.out.println("A 27 nem oszthato 10-zel vagy 3-mal maradeknelkul");
+      }
       
-        else
-            System.out.println("A szám negatív");
-        
-        boolean feltetel = (szam >=0);
-        if(feltetel) {
-            System.out.println("A szám negatív");
-        }
-           else {
-                   System.out.println("A szam pozitiv");
-                 }
-        System.out.println("Kerek egy szamot!");
-        Scanner be =new Scanner(System.in);
-        int a = be.nextInt();
-        System.out.println("Kerek egy masik szamt!");
-        int b = be.nextInt();
-        if (a<b) {
-            System.out.printf("A %d nagyobb, mint %d", b, a);
-        }
-        else {
-            System.out.printf("A %d nagyobb ,mint %d",a,b);
-        }
-        
-        
-        }
+      double szazalek = 60.0;
+      // 0-39 elégtelen
+      // 40-49 elégséges
+      // 50-59 közepes
+      // 60-79 jó
+      // 80-100 jeles
+      if (szazalek>=80){
+          System.out.println("Jeles");
+      }
+      else if (szazalek >= 60 && szazalek < 80){
+        System.out.println("jo");
+       }
+      else if (szazalek >= 50 && szazalek < 60){
+        System.out.println("kozepes");
+      }
+      else if (szazalek >= 40 && szazalek <50){
+          System.out.println("elegseges");
+      }
+      else if (szazalek >= 0 && szazalek < 40) {
+          System.out.println("elegtelen");
+      }
+      
     }
+
+
+
+}
+
 
                
      
